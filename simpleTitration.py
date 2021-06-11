3d52798c656a2fa7c1e03ce7dc836339a641ca77
Mark Schatza <markschatza@gmail.com>
2021-06-11 13:49:30 -0500

    Updated math in auto titration

--- simpleTitration.py
+++ simpleTitration.py
@@ -1,17 +1,20 @@
 import zmqClasses
 import stimmer
 import daqAPI
+import pandas as pd
+import numpy as np
+import os
+from statistics import mean
 
 ######################### THINGS TO CHANGE ###########################
-nERP = 20                 # EP per uA
+nERP = 4                 # EP per uA
 stimAddressX = 'Dev1/ao0' # stim
 startuA = 100             # uA to start at
-enduA = 300               # uA to end at
+enduA = 175               # uA to end at
 stepSize = 25             # uA step size between start and end
 OE_Address = "localhost"  # localhost or ip address of other computer
 record_dir = ""           # Where do you want data to be recorded
 #############################################################
-
 
 snd = zmqClasses.SNDEvent(OE_Address,5556, recordingDir = record_dir)
 print('IF HANGS ADD NETWORK EVENTS TO OPEN EPHYS!!!')
@@ -25,11 +28,20 @@ snd.send(snd.STOP_ACQ)
 
 if(os.path.exists("E:\\test\\example.csv")):
     df = pd.read_csv("E:\\test\\example.csv")
-    k = df.shape
 
 if(~df.empty):
-    ab = ((df.max(axis=1)-(df.max(axis=1)).max()*0.5).abs()).idxmin()
-    selectedStimStep = int(ab/nERP)
-    uAList = np.arange(startuA, enduA, stepSize)
-    print('Stimulation at 50% value , ' + uAList[selectedStimStep])
+    splitdf = np.array_split(df,nERP) # Split into different amplitudes
+    uAList = np.arange(startuA, enduA+1, stepSize) # How many steps
+    ERPList = [0] * len(uAList)
+    curMax = 0
+    for i in range(len(uAList)):
+        ERPList[i] = np.mean(splitdf[i], axis = 0)[0]
+
+    halfAmp = max(ERPList) / 2 # What is our 50% amplitude
+    index = np.argmin(np.abs(np.array(ERPList)-halfAmp))
+    print(np.abs(np.array(ERPList)-halfAmp))
+    print(ERPList)
+    print(halfAmp)
+    print(index)
+    print('uA stimulation at 50% value : ' + str(uAList[index]))
 
